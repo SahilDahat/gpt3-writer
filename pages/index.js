c82ea57d384395eb399c632a24a45761feb97fa6
Sahil Dahat <sahil.developerinfo@gmail.com>
2023-07-19 01:13:14 +0530

    Older code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import buildspaceLogo from '../assets/buildspace-logo.png'
 
 const Home = () => {
   const [userInput, setUserInput] = useState('')
